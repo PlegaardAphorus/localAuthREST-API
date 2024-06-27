a8f7c4877b5c6cfc9e416eb1ebbe681eb695b364
Smartworx Praktikanten <Praktikant@smartworx.de>
2024-06-27 14:22:59 +0200

    .gitignore hinzugefügt für .vs Ordner, ToDo API fertig gestellt

--- LocalAuthREST-API/LocalAuthREST-API/Program.cs
+++ LocalAuthREST-API/LocalAuthREST-API/Program.cs
@@ -10,7 +10,15 @@ app.MapGet("/todoitems", async (TodoDb db) => await db.todos.ToListAsync());
 
 app.MapGet("/todoitems/complete", async (TodoDb db) => await db.todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapPost("/todoitems/{id}", async (int id, TodoDb db) => await db.todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) => await db.todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
 app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 {
@@ -26,7 +34,7 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
     return Results.NoContent();
 });
 
-app.MapDelete("/todoitems{id}", async (int id, TodoDb db) =>
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 {
     if (await db.todos.FindAsync(id) is Todo todo)
     {
