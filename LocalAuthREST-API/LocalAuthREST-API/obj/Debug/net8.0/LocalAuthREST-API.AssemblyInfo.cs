64eadadc06eb8f341a4f6abc3cc2f4da6d88fc18
Smartworx Praktikanten <Praktikant@smartworx.de>
2024-07-03 10:36:09 +0200

    Cache Update

--- LocalAuthREST-API/LocalAuthREST-API/obj/Debug/net8.0/LocalAuthREST-API.AssemblyInfo.cs
+++ LocalAuthREST-API/LocalAuthREST-API/obj/Debug/net8.0/LocalAuthREST-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalAuthREST-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53721a3193944a7fceb9c29e3c1f59d1263aa92f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970e0cef2ca4d119d73eef9b086b4d7fd6c73714")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalAuthREST-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalAuthREST-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
